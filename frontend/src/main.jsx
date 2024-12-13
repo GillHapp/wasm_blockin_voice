a557570d6a56988c668a93a22b2700de275de33f
harpreet singh <happybiostockcode07040@gmail.com>
2024-12-13 13:19:47 +0530

    first commit

---
+++ frontend/src/main.jsx
@@ -0,0 +1,15 @@
+"use client";
+import { AbstraxionProvider } from "@burnt-labs/abstraxion";
+import "@burnt-labs/abstraxion/dist/index.css";
+import "@burnt-labs/ui/dist/index.css";
+
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
